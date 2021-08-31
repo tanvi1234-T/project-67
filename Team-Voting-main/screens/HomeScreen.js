972d5e0d903e0ab1653dd684930ed8f4f4d89c7f
tanvi1234-T <75284589+tanvi1234-T@users.noreply.github.com>
2021-08-31 19:41:32 -0400

    Add files via upload

---
+++ Team-Voting-main/screens/HomeScreen.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Header,
+  Image,
+} from 'react-native';
+
+import AppHeader from '../components/AppHeader';
+import db from '../config';
+
+export default class HomeScreen extends Component {
+
+  teamA(){
+   db.ref('/').update({
+     'teamA':1
+   })
+ }
+
+ teamB(){
+   console.log(db);
+   db.ref('/').update({
+     'teamB':2
+   })
+ }
+
+  render() {
+    return (
+      <View>
+        <AppHeader />
+        <View>
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity>
+              <Image
+                style={{ width: 300, height: 220, marginLeft: 5 }}
+                source={require('../assets/TeamImage.png')}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.ratingContainer}>
+            <Text style={{ textAlign: 'center',fontSize:25 }}>Vote Here</Text>
+            <TouchableOpacity
+              style={styles.buttons}
+              onPress ={this.teamA()}>
+              <Text style={{ fontSize:20}}>Team A</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.buttons}
+              onPress ={this.teamB()}>
+              <Text style={{ fontSize:20}}>Team B</Text>
+            </TouchableOpacity>
+
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  buttonsContainer: {
+    alignSelf: 'center',
+    marginTop: 30,
+  },
+  buttons: {
+    backgroundColor:"coral",
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 4,
+    borderRadius: 10,
+    margin:9,
+    width: 120,
+    height: 20,
+  },
+  ratingContainer: {
+    alignSelf: 'center',
+    marginTop: 60,
+  },
+});
